b82d3dcc3b4de3e09f9a8f7411635ec220ccac70
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-03-01 20:21:45 -0300

    create context

---
+++ src/context/placesProvider.tsx
@@ -0,0 +1,18 @@
+export interface PlacesStates {
+  isLoading: boolean;
+  userLocation?: [number, number];
+}
+
+
+const INITIAL_STATE: PlacesStates = {
+
+    isLoading:false,
+    userLocation: undefined
+
+}
+
+const placesProvider = () =>{
+
+
+    return{}
+}
