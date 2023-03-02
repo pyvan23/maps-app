1f795a89cb8e795a6aba7cf931aa0110882adfb2
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-03-02 20:52:40 -0300

    html

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1>Maps App</h1>
+       <h3>description:</h3>
       </header>
     </div>
   );
--- src/context/placesProvider.ts
+++ src/context/placesProvider.ts
@@ -4,7 +4,7 @@
 
 export const placesProvider = () => {
 
-    return 
+    return {}
     
   
 }
