cda49cef2b07f78e96fc6bee0f2ae40c50c58967
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-03-06 19:01:38 -0300

    changes folders, and createmaps context maps again

--- src/context/places/placesContext.ts
+++ src/context/places/placesContext.ts
@@ -1,8 +1,10 @@
 import { createContext } from "react";
 
+export interface PlacesContextProps {
+  isLoading: boolean;
+  userLocation?: [number, number];
+}
 
+export const placesContext = createContext<PlacesContextProps>(
+  {} as PlacesContextProps);
 
-
-
-
-export const placesContext = createContext({})
