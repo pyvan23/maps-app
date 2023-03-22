4e253c3d2149e1ce8f181d9a18612fb99a6c9006
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-03-22 18:55:01 -0300

    npm start up

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
+import mapboxgl from 'mapbox-gl'; 
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoicHl2YW4iLCJhIjoiY2xhM3ZwZGZoMHB6ODNvcnBnNDF1ODB6diJ9.XQSkbnw6uQSd9LSANMQKLQ';
 
 if (!navigator.geolocation) {
   alert('Tu navergador no tiene a opcion de acceder a la geoocalizacion')
