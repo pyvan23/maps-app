92f3453100d0cd8ca02a162d229404d523a289b4
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-03-22 21:50:16 -0300

    mapView

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,35 +1,48 @@
-import { useContext } from "react";
+import { useContext, useLayoutEffect, useRef } from "react";
 import { PlacesContext } from "../context/places/PlacesContext";
+import { Map } from "mapbox-gl";
 
 export const MapView = () => {
 
     const { userLocation, isLoading } = useContext(PlacesContext);
+    const mapDiv = useRef<HTMLDivElement>(null);
 
+    //
+    useLayoutEffect(() => {
 
-    if (isLoading) {
+        if ( !isLoading ) {
+            const map = new Map({
+                container: 'map', // container ID
+                style: 'mapbox://styles/mapbox/streets-v12', // style URL
+                center: [-74.5, 40], // starting position [lng, lat]
+                zoom: 9, // starting zoom
+           });
+        }
+
+    }, [isLoading]);
+
+    if ( isLoading ) {
 
         return (
             <div className="loading-map d-flex justify-content-center">
-
                 <div className=" spinner-border" role="status">
                     <span className="visually-hidden">Loading...</span>
-
                 </div>
                 <div>
                     <h4>Loading...</h4>
                 </div>
-
-
             </div>
         )
-    }
-
-
+    };
     return (
 
-        <>
-            <h3>geolocalizacion del usuario: {userLocation}</h3>
-        </>
-
+        <div ref={mapDiv}
+            style={{
+                height: '100vh',
+                width: '100vw', position: "fixed",
+                top: '0', left: '0'
+            }}>
+            {userLocation}
+        </div>
     );
 };
