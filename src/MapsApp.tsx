e9549926e4000b82f789e6daa16762a8bcb7309c
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-03-25 13:00:34 -0300

    testing fecth search button

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -1,11 +1,13 @@
 
+import AppFecth from './components/fecth';
 import { PlacesProvider } from './context/places/PlacesProvider'
 import { HomeScreen } from './screens/HomeScreen';
 import './styles.css'
 export const MapsApp = () => {
   return (
     <PlacesProvider>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <AppFecth/>
     </PlacesProvider>
   )
 }
--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -8,18 +8,18 @@ export const MapView = () => {
     const mapDiv = useRef<HTMLDivElement>(null);
 
     //
-    useLayoutEffect(() => {
+    // useLayoutEffect(() => {
 
-        if ( !isLoading ) {
-            const map = new Map({
-                container: 'map', // container ID
-                style: 'mapbox://styles/mapbox/streets-v12', // style URL
-                center: [-74.5, 40], // starting position [lng, lat]
-                zoom: 9, // starting zoom
-           });
-        }
+    //     if ( !isLoading ) {
+    //         const map = new Map({
+    //             container: , // container ID
+    //             style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    //             center: [-74.5, 40], // starting position [lng, lat]
+    //             zoom: 9, // starting zoom
+    //        });
+    //     }
 
-    }, [isLoading]);
+    // }, [isLoading]);
 
     if ( isLoading ) {
 
